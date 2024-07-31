49707d60321b360417c64885cb79af24f14ed5fe
DonateloXL <joao.pedro_gv24@outlook.com>
2024-07-31 03:30:03 -0300

    commit da tesla

---
+++ script.js
@@ -0,0 +1,14 @@
+let formulário = document.querySelector(".formulário")
+
+console.log(formulário)
+
+function aparecerFormulário(){
+    formulário.style.left ="38%"
+
+}
+
+function desaparecerFormulário(){
+    formulário.style.left ="-50%"
+}
+
+alert("Bem-Vindo ao meu primeiro projeto")
